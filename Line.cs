6035ead1edfe2ecf5dca56bb2a483bf6761383a7
dnyana <dnyaneshgawade903@gmail.com>
2021-07-21 20:01:29 +0530

    [Dnyanesh] add: comparison of two lines

--- Line.cs
+++ Line.cs
@@ -6,21 +6,34 @@ namespace LineComparison
 {
     public class Line
     {
-        public int x1, x2, y1, y2;
-        public Line(int x1,int x2,int y1,int y2)
+        public int x1, x2, x3, x4, y1, y2,y3,y4;
+        public Line(int x1,int x2, int x3, int x4,int y1,int y2, int y3, int y4)
         {
             this.x1 = x1;
             this.x2 = x2;
+            this.x3 = x3;
+            this.x2 = x4;
             this.y1 = y1;
             this.y2 = y2;
+            this.y3 = y3;
+            this.y4 = y4;
 
         }
 
         public void calculate()
         {
-            double lenth = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("the length of the line is:"+lenth);
+            double lenth1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("the length of the first line is:"+lenth1);
+            double lenth2 = Math.Sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+            Console.WriteLine("the length of the second line is:" + lenth2);
+            if (lenth1>lenth2)
+                Console.WriteLine("first line is greter");
+            else if(lenth1>lenth2)
+                Console.WriteLine("second line is greter");
+            else
+                Console.WriteLine("both lines are equal");
         }
-        
+       
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,15 +7,14 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to the line comparison");
-            Line line = new Line(1,3,2,4);
-            Line line2 = new Line(2,3,4,5);
+            Line line = new Line(1,2,3,4,5,6,7,8);
+            
 
             line.calculate();
-            line2.calculate();
-            if (line.Equals(line2))
-                Console.WriteLine("both lines are same size");
-            else
-                Console.WriteLine("both lines are different sizes");
+        
+           
+           
+
 
         }
     }
