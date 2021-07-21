1ca48a6433384a331b0a1168b53c2dee9ca89f0f
dnyana <dnyaneshgawade903@gmail.com>
2021-07-21 10:35:40 +0530

    [Dnyanesh] add: checking the are equal or not

--- Program.cs
+++ Program.cs
@@ -8,7 +8,14 @@ namespace LineComparison
         {
             Console.WriteLine("Wellcome to the line comparison");
             Line line = new Line(1,3,2,4);
+            Line line2 = new Line(2,3,4,5);
+
             line.calculate();
+            line2.calculate();
+            if (line.Equals(line2))
+                Console.WriteLine("both lines are same size");
+            else
+                Console.WriteLine("both lines are different sizes");
 
         }
     }
