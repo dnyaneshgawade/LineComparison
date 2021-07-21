36b06b3d7fe0608b32fdc5e581559f839f437dfd
dnyana <dnyaneshgawade903@gmail.com>
2021-07-21 10:23:33 +0530

    [Dnyanesh] add: length of the line

---
+++ Line.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    public class Line
+    {
+        public int x1, x2, y1, y2;
+        public Line(int x1,int x2,int y1,int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+
+        }
+
+        public void calculate()
+        {
+            double lenth = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("the length of the line is:"+lenth);
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to the line comparison");
+            Line line = new Line(1,3,2,4);
+            line.calculate();
+
         }
     }
 }
